0b28c37118f859b4c836f9e769f7f031ff6f117e
AlaskaJay <thatthinker@gmail.com>
2018-03-22 19:31:01 -0400

    Adding actual project files
    Needs Implementation

---
+++ DangersAndDungeons/DangersAndDungeons/Model/Floor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DangersAndDungeons.Model
+{
+    class Floor : Tile
+    {
+        private char DEFAULT = 'O';
+        private List<Item> stash;
+
+        public char toChar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string toString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
