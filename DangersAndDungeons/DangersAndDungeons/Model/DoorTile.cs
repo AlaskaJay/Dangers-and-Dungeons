12f2f94d431f6fb226d57d06c410b38eea29d3fb
AlaskaJay <thatthinker@gmail.com>
2018-03-30 15:23:39 -0400

    Fleshed out Tiles

---
+++ DangersAndDungeons/DangersAndDungeons/Model/DoorTile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DangersAndDungeons.Model
+{
+    class DoorTile : Tile 
+    {
+        private char DEFAULT = 'D';
+        private bool locked;
+
+        public char toChar()
+        {
+            return DEFAULT;
+        }
+
+        public string toString()
+        {
+            return DEFAULT + "";
+        }
+    }
+}
---
+++ DangersAndDungeons/DangersAndDungeons/Model/FloorTile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DangersAndDungeons.Model
+{
+    class FloorTile : Tile
+    {
+        private char DEFAULT = '.';
+        private List<Item> stash;
+
+        public char toChar()
+        {
+            return DEFAULT;
+        }
+
+        public string toString()
+        {
+            return DEFAULT + "";
+        }
+    }
+}
